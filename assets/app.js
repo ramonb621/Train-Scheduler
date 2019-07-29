a696e2781b38828a11e1ccc64bcdc4294e584ead
Ramon Barajas <ramonb621@yahoo.com>
2019-07-29 15:36:01 -0700

    got moment JS to work with firebase

--- assets/app.js
+++ assets/app.js
@@ -17,37 +17,49 @@ var destination = "";
 var firstTrain = "";
 var frequency = 0;
 
-var diffTime = moment().diff(moment(firstTrain), "minutes");
-var timeApart = diffTime % frequency;
-var minutesNextTrain = frequency - timeApart;
-var nextTrain = moment().add(minutesNextTrain, "minutes");
+// var diffTime = moment().diff(moment(firstTrain), "minutes");
+// var timeApart = diffTime % frequency;
+// var minutesNextTrain = frequency - timeApart;
+// var nextTrain = moment().add(minutesNextTrain, "minutes");
 
 $("#submit").on('click', function (event) {
     event.preventDefault();
     trainName = $("#train-name").val().trim();
     destination = $("#destination").val().trim();
-    firstTrain = $("#first-train-time").val().trim();
+    firstTrain = moment($("#first-train-time").val().trim(), "HH:mm").subtract(10, "years").format("X");
     frequency = $("#frequency").val().trim();
 
 
     var addTrain = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destination),
+        $("<td>").text(firstTrain),
         $("<td>").text(frequency),
-        $("<td>").text(nextTrain),
-        $("<td>").text(minutesNextTrain),
+        $("<td>").text(),
     );
     
     $("tbody").append(addTrain);
     console.log(addTrain);
   database.ref().push(addTrain);
 })
-database.ref().on("child_added", function(snapshot){
-    var sv = snapshot.val();
+database.ref().on("child_added", function(childSnapshot){
+    var sv = childSnapshot.val();
+
+    var name = sv.trainName;
+    var trainDestination = sv.destination;
+    var first = sv.firstTrain;
+    var freq = sv.frequency;
 
     console.log(sv);
     console.log(sv.trainName);
     console.log(sv.destination);
     console.log(sv.firstTrain);
     console.log(sv.frequency);
+
+    let remainder = moment().diff(moment.unix(first), "minutes") % freq;
+    let minutes = freq - remainder;
+
+    let trainArrival = moment().add(minutes, "m").format("hh:mm A");
+
+    $("tbody").append("<tr><td>" + name + "</td><td>" + trainDestination + "</td><td class = 'min'>" + freq + "</td><td class = 'min'>" + trainArrival + "</td><td class = 'min'>" + minutes + "</td></td>");
   })
